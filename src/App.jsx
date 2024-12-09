26d6cf205228534735049e8e72186c5ea5b27a0b
Dhafin <dhafinqr@gmail.com>
2024-12-09 13:55:44 +0700

    feat : add state to change tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
+import {useState} from "react";
 import logo from "./assets/Vector.png"
 import './App.css'
 import NavItems from "./components/NavItems"
 
 function App() {
-  
+  const [activeTab, setActiveTab] = useState("Dashboard");
   const menu = [
     {
       title: "Dashboard",
-      link: "#"
+      link: "#",
     },
     {
       title: "Transfer",
-      link: "#"
+      link: "#",
     },
     {
       title: "Top Up",
-      link: "#"
+      link: "#",
     },
     {
       title: "Sign Out",
-      link: "#"
+      link: "#",
     },
   ]
   
   return (
     <>
-    <nav className = "flex items-center w-screen justify-between bg-white py-4 px-8">
-      <img src={logo}/>
-      <NavItems menu={menu}/>
+    <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
+      <img src={logo} />
+      <NavItems menu={menu} activeTab={activeTab} handleClick={setActiveTab} />
     </nav>
     </>
   )
--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,8 +1,8 @@
-function NavItems({menu}) {
+function NavItems({menu, activeTab, handleClick}) {
     return(
         <div className = "flex gap-x-8">
             {menu.map((item, index) => (
-                <a key={index} href={item.link}>{item.title}</a>
+                <a onClick = {() => handleClick(item.title)} className={activeTab !== item.title ? 'text-black' : "text [#19918f] font-bold"} key={index} href={item.link}>{item.title}</a>
             ))}
         </div>
     )
