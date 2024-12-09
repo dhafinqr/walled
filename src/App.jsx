1cb6ea4cb9e12e73a45355834581c2483ff50040
Dhafin <dhafinqr@gmail.com>
2024-12-09 12:00:47 +0700

    feat: add ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from "./assets/Vector.png"
 import './App.css'
+import NavItems from "./components/NavItems"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#"
+    },
+    {
+      title: "Transfer",
+      link: "#"
+    },
+    {
+      title: "Top Up",
+      link: "#"
+    },
+    {
+      title: "Sign Out",
+      link: "#"
+    },
+  ]
+  
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <nav className = "flex items-center w-screen justify-between bg-white py-4 px-8">
+      <img src={logo}/>
+      <NavItems menu={menu}/>
+    </nav>
     </>
   )
 }
